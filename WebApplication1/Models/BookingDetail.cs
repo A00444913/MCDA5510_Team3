1d86fb67b2230a248e865545be462e7ef86e8105
a00442518 <gurpartap.singh.gurpartap.singh@smu.ca>
2021-04-05 15:58:02 +0530

    hotel-site

---
+++ WebApplication1/Models/BookingDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WebApplication1.Models
+{
+    public partial class BookingDetail
+    {
+        public int Cid { get; set; }
+        public int Uid { get; set; }
+        public int Rid { get; set; }
+        public int Hid { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int GuestCount { get; set; }
+        public string Status { get; set; }
+    }
+}
