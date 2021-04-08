8c4c44a0668e9a77bb612c1844dd6a6d3aecdc89
A00444913 <Jinting.Yang@smu.ca>
2021-04-08 18:59:09 +0800

    [view] find room page
    add find room page

---
+++ WebApplication1/Controllers/RoomController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Findroom()
+        {
+            return View();
+        }
+
+        public IActionResult Roomlist()
+        {
+            return View();
+        }
+    }
+}
