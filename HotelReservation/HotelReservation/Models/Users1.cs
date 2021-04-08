6a2f91b48ef8d7308865beb46c3a7968f04347cb
a00442518 <gurpartap.singh.gurpartap.singh@smu.ca>
2021-04-08 18:32:36 +0530

    HotelReservation

---
+++ HotelReservation/HotelReservation/Models/Users1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace HotelReservation.Models
+{
+    public partial class Users1
+    {
+        public Users1()
+        {
+            Bookings1s = new HashSet<Bookings1>();
+        }
+
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string ConfirmEmail { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public virtual ICollection<Bookings1> Bookings1s { get; set; }
+    }
+}
