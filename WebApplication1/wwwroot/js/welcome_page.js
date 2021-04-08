6b4a495d0fb5fa0663d9e90799356e6ec11de61a
A00444913 <Jinting.Yang@smu.ca>
2021-04-08 11:45:09 +0800

    [view] welcome page

--- WebApplication1/wwwroot/js/welcome_page.js
+++ WebApplication1/wwwroot/js/welcome_page.js
@@ -1,1 +1,14 @@
-﻿
+﻿class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <img src="../images/header.png" />
+                <img src="../images/hotel.png" width="1500px" />
+            </div>
+            )
+    }
+}
+ReactDOM.render(<App />, document.getElementById("home"))
