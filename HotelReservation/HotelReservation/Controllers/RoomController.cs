e3bc7b1efb4d403eee38f8e91cbd670c86f1ffdd
A00444913 <Jinting.Yang@smu.ca>
2021-04-09 09:24:43 +0800

    Adjust registration page ui

--- HotelReservation/HotelReservation/Controllers/RoomController.cs
+++ HotelReservation/HotelReservation/Controllers/RoomController.cs
@@ -3,15 +3,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HotelReservation.Models;
 
 namespace HotelReservation.Controllers
 {
     public class RoomController : Controller
     {
+        private readonly hotelContext _db;
+
+        /*public RoomController(hotelContext db)
+        {
+            _db = db;
+        }*/
+
         public IActionResult Findroom()
         {
             return View();
         }
+
+        //private readonly hotelContext _db;
 
         public IActionResult Roomlist()
         {
